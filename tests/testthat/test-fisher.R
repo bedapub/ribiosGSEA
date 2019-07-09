fc477b9255d17c7fe3caaf857bebe5dbc161ec70
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-07-09 14:41:56 +0200

    category is replaced by namespace, and GeneSet replaced by GmtList

--- tests/testthat/test-fisher.R
+++ tests/testthat/test-fisher.R
@@ -25,12 +25,12 @@ badGenes <- function(vector, dup=2, naCount=3, sample=FALSE) {
     return(res)
 }
 
-gsEnrich1 <- ribiosGSEA::fisherTest(inputGenes, geneSet1, universe, "testName", "testCategory")
+gsEnrich1 <- ribiosGSEA::fisherTest(inputGenes, geneSet1, universe, "testName", "testNamespace")
 gsManual1 <- fisher.test(matrix(c(3,3,0,20), nrow=2, byrow=TRUE), alternative="greater")
 expect_equal(gsManual1$p.value, ribiosGSEA::pValue(gsEnrich1))
 expect_equal(length(geneSet1), ribiosGSEA::gsEffSize(gsEnrich1))
 expect_that(inputGenes, equals(ribiosGSEA::hits(gsEnrich1)))
-expect_that(gsCategory(gsEnrich1), equals("testCategory"))
+expect_that(gsNamespace(gsEnrich1), equals("testNamespace"))
 expect_that(gsName(gsEnrich1), equals("testName"))
             
 gsEnrich2 <- ribiosGSEA::fisherTest(inputGenes, geneSet2, universe)
@@ -56,7 +56,7 @@ expect_that(c("B", "C"), equals(ribiosGSEA::hits(gsEnrich4)))
 ##----------------------------------------##
 ## test fisherTest for character, character, character
 ##----------------------------------------##
-gsS4.1 <- fisherTest(inputGenes, geneSet1, universe, "testName", "testCategory")
+gsS4.1 <- fisherTest(inputGenes, geneSet1, universe, "testName", "testNamespace")
 expect_identical(gsS4.1, gsEnrich1)
 
 gsS4.2 <- fisherTest(inputGenes, geneSet2, universe)
@@ -70,7 +70,7 @@ expect_identical(gsS4.4, gsEnrich4)
 
 #### manipulate input genes
 ## test that both redundant and NA genes are removed
-gsS4.1.bad <- fisherTest(badGenes(inputGenes), geneSet1, universe, "testName", "testCategory")
+gsS4.1.bad <- fisherTest(badGenes(inputGenes), geneSet1, universe, "testName", "testNamespace")
 expect_identical(gsS4.1.bad, gsS4.1)
 
 #### manipulate gene sets
@@ -82,27 +82,27 @@ gsS4.4.ubad <- fisherTest(badGenes(inputGenes), badGenes(geneSet4), badGenes(uni
 expect_identical(gsS4.4.ubad, gsS4.4)
 
 ##----------------------------------------##
-## test fisherTest for character, GeneSet, character
+## test fisherTest for character, list, character
 ##----------------------------------------##
-myGeneSet4 <- new("GeneSet", category="Bundesliga", name="Hamburger Sportverein", desc="some description", genes=geneSet4)
+myGeneSet4 <- list(name="Hamburger Sportverein", desc="some description", genes=geneSet4, namespace="Bundesliga")
 myGeneSet4.fisher <- fisherTest(badGenes(inputGenes), myGeneSet4, badGenes(universe))
 expect_that(pValue(myGeneSet4.fisher), equals(pValue(gsS4.4)))
 expect_that(gsName(myGeneSet4.fisher), equals("Hamburger Sportverein"))
-expect_that(gsCategory(myGeneSet4.fisher), equals("Bundesliga"))
+expect_that(gsNamespace(myGeneSet4.fisher), equals("Bundesliga"))
 
 ##----------------------------------------##
-## test fisherTest for character, GeneSets, character
+## test fisherTest for character, GmtList, character
 ##----------------------------------------##
-gs1 <- new("GeneSet", category="A", name="GeneSet1", genes=geneSet1)
-gs2 <- new("GeneSet", category="A", name="GeneSet2", genes=geneSet2)
-gs3 <- new("GeneSet", category="A", name="GeneSet3", genes=geneSet3)
-gs4 <- new("GeneSet", category="B", name="GeneSet4", genes=geneSet4)
-gss <- GeneSets(list(gs1, gs2, gs3, gs4))
+gs1 <- list(name="GeneSet1", desc="", genes=geneSet1, namespace="A")
+gs2 <- list(name="GeneSet2", desc="", genes=geneSet2, namespace="A")
+gs3 <- list(name="GeneSet3", desc="", genes=geneSet3, namespace="A")
+gs4 <- list(name="GeneSet4", desc="", genes=geneSet4, namespace="B")
+gss <- GmtList(list(gs1, gs2, gs3, gs4))
 myFisherRes <- fisherTest(inputGenes, gss, universe)
 
 myFisherRes.expP <- c(pValue(gsEnrich1),pValue(gsEnrich2),pValue(gsEnrich3),pValue(gsEnrich4))
 expect_that(myFisherRes$PValue, equals(myFisherRes.expP))
-expect_that(myFisherRes$GeneSetCategory, equals(c(rep("A", 3), "B")))
+expect_that(myFisherRes$GeneSetNamespace, equals(c(rep("A", 3), "B")))
 expect_that(myFisherRes$GeneSetName, equals(sprintf("GeneSet%d", 1:4)))
 
 myFisherRes.bad <- fisherTest(badGenes(inputGenes), gss, badGenes(universe))
