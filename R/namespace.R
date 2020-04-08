eb46f674495abcab6f904f37498e8eda419d0e74
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-04-08 16:33:29 +0200

    add namespaces from basic packages

--- R/namespace.R
+++ R/namespace.R
@@ -1,4 +1,8 @@
-#' @importFrom methods setClass setGeneric setMethod
+#' @importFrom grDevices colorRampPalette
+#' @importFrom graphics plot
+#' @impoftFrom stats ave cor filter fisher.test kmeans median p.adjust pchisq phyper pnorm pt qt rnorm runif sd t.test update var
+#' @importFrom utils read.table
+#' @importFrom methods setClass setGeneric setMethod as is new callGeneric
 #' @importFrom XML xmlAttrs xmlTreeParse xmlRoot xmlChildren xmlName
 #' @importFrom parallel mclapply
 #' @importFrom Rcpp evalCpp
