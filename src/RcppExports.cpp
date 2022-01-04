8226ec051a147cdf515e19f363960bda97764c1f
Jitao David Zhang <jitao_david.zhang@roche.com>
2022-01-04 16:55:23 +0100

    add orderByNumberInStr and factorByNumberInStr

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // cpp_geneSetPerm
 RcppExport SEXP cpp_geneSetPerm(SEXP stats, SEXP rinds, SEXP Nsim);
 RcppExport SEXP _ribiosGSEA_cpp_geneSetPerm(SEXP statsSEXP, SEXP rindsSEXP, SEXP NsimSEXP) {
--- tests/testthat/test_strWithNumber.R
+++ tests/testthat/test_strWithNumber.R
@@ -6,15 +6,15 @@ context("Test strWithNumber")
 days <- c("D1", "D10", "D15", "D3.5")
 
 test_that("orderByNumberInStr works", {
-    expect_identical(orderByNumberInStr(days),
-		     c(3, 2, 4, 1))
+    expect_identical(orderByNumberInStr(days, decreasing=TRUE),
+		     c(3L, 2L, 4L, 1L))
     expect_identical(orderByNumberInStr(days, decreasing=FALSE),
-		     c(1, 4, 2, 3))
+		     c(1L, 4L, 2L, 3L))
 })
 
 test_that("factorByNumberInStr works", {
     expect_identical(factorByNumberInStr(days, decreasing=FALSE),
 		     factor(days, levels=c("D1", "D3.5", "D10", "D15")))
-    expect_identical(orderByNumberInStr(days, decreasing=TRUE),
+    expect_identical(factorByNumberInStr(days, decreasing=TRUE),
 		     factor(days, levels=c("D15", "D10", "D3.5", "D1")))
 })
